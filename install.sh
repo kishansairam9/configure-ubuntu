9d11423c43bae7ba8d63d2e965c370eb14d67efc
Kishan Sairam Adapa <kishansairam.adapa9@gmail.com>
2019-07-30 12:33:35 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,21 +1,21 @@
 #!/usr/bin/env bash
 
 echo "Installing using apt-get"
-chmod u+x ./scripts/from_apt-get.sh
+chmod x ./scripts/from_apt-get.sh
 ./scripts/from_apt-get.sh
 clear
 echo "Installing using snap"
-chmod u+x ./scripts/from_snap.sh
+chmod x ./scripts/from_snap.sh
 ./scripts/from_snap.sh
 clear
 echo "Installing Multi-Touch Gestures"
-#chmod u+x ./scripts/multitouch-gestures.sh
-#./scripts/multitouch-gestures.sh
-#clear
+chmod +x ./scripts/multitouch-gestures.sh
+./scripts/multitouch-gestures.sh
+clear
 #echo "Installing OneDrive"
 #chmod u+x ./scripts/onedrive-install.sh
 #./scripts/onedrive-install.sh
 #clear
 echo "To be Done"
-chmod u+x ./scripts/to-be-done.sh
+chmod +x ./scripts/to-be-done.sh
 ./scripts/to-be-done.sh
